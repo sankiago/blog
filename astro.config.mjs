b476ec9a3e22666e77fdab0bbcdaf9ba3c36df4d
sankiago <lpzsds@gmail.com>
2025-12-18 15:32:22 -0500

    FEAT: Adding blog index, correcting styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// // @ts-check
+// import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// // https://astro.build/config
+// export default defineConfig({});
+
+ import { defineConfig } from 'astro/config';
+ import remarkMath from 'remark-math';
+ import rehypeKatex from 'rehype-katex';
+
+ export default defineConfig({
+   markdown: {
+     remarkPlugins: [remarkMath],
+     rehypePlugins: [rehypeKatex],
+   },
+ });
